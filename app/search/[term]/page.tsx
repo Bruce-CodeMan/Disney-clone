1120889244e485ce23a80c6060d17c995e7e4e6d
Bruce Hsu <1033684650@qq.com>
2023-12-30 16:57:14 +0800

    finish the whole project

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -4,6 +4,8 @@
  * @Description: 
  */
 
+import MoviesCarousel from "@/components/MoviesCarousel"
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies"
 import { notFound } from "next/navigation"
 
 interface Props {
@@ -12,17 +14,27 @@ interface Props {
   }
 }
 
-const SearchPage = ({params}: Props) => {
+const SearchPage = async ({params}: Props) => {
 
   if(!params.term) notFound()
 
   const termUse = decodeURI(params.term)
 
   // API call to get the searched movies
+  const movies = await getSearchedMovies(termUse)
+  const popularMovies = await getPopularMovies();
+
 
 
   return (
-    <div>Search Page, {termUse}</div>
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termUse}</h1>
+
+        <MoviesCarousel title="Movies" movies={movies} isVertical/>
+        <MoviesCarousel title="You may be alse like" movies={popularMovies}/>
+      </div>
+    </div>
   )
 }
 
